88c85a31170dc11e99d9221f42ea0c7305424b11
Ptitlu42 <lucas.beyer@gmx.fr>
2024-05-29 21:00:10 +0200

    Clean

--- sms.py
+++ sms.py
@@ -1,22 +1,21 @@
-import os
 from twilio.rest import Client
 
-from config import account_sid, auth_token, twilio_number, destination_number
+from config import account_sid, auth_token, destination_number
 
 client = Client(account_sid, auth_token)
 
-sendby=""
-message=""
+SENDBY = ""
+SMS_BODY = ""
 
-def send_sms(body, to=destination_number, from_=sendby):
-    message = client.messages.create(
+def send_sms(body, to=destination_number, from_=SENDBY):
+    msg = client.messages.create(
         body=body,
         from_=from_,
         to=to
     )
-    return message.sid
+    return msg.sid
 
 if __name__ == "__main__":
-    sms_body = message
+    sms_body = SMS_BODY
     message_sid = send_sms(sms_body)
-
+    print(f"Sent message with SID: {message_sid}")
