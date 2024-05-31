edba52df3cd7a9dc75c0e36dd4fac1a0f6436e6c
Ptitlu42 <lucas.beyer@gmx.fr>
2024-05-31 13:59:31 +0200

    Base app

--- poop_settings/urls.py
+++ poop_settings/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for daily_poop project.
+URL configuration for poop_settings project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
