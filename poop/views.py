ddffe2b46641283980e48b6d5339bd7ad1eaf188
Ptitlu42 <lucas.beyer@gmx.fr>
2024-06-02 12:14:39 +0200

    list users page

--- poop/views.py
+++ poop/views.py
@@ -5,6 +5,10 @@ from poop.models import User
 def welcome(request):
     return render(request, 'poop/welcome.html')
 
-def show_users(request, phone_number):
+def show_user(request, phone_number):
     user = get_object_or_404(User, phone_number=phone_number)
     return render (request, 'poop/user.html', {'user': user})
+
+def show_users(request):
+    users = User.objects.all()
+    return render (request, 'poop/users.html', {'users': users})
